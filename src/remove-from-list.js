321df9d2d6c03832e53417ad2a3a6ede0377b404
40iq <maksPTD6@mail.ru>
2023-12-13 23:55:25 +0300

    feat: finish basic-JS-DS task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+
+  let nodeList = l;
+
+  while (nodeList !== null) {
+    nodeList.next !== null && nodeList.next.value === k
+      ? (nodeList.next = nodeList.next.next)
+      : (nodeList = nodeList.next);
+  }
+
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
